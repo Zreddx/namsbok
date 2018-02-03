2caacbaaa765286a0a7afbca04f5198e294e0fa3
Zreddx <xzlydez@gmail.com>
2018-02-03 15:22:50 +0000

    Uploaded base files
    Feel free to submit changes

---
+++ first5.php
@@ -0,0 +1,19 @@
+<?php
+@$name = $_FILES['file']['name'];
+@$size = $_FILES['file']['size'];
+@$type = $_FILES['file']['type'];
+@$tmp_name = $_FILES['file']['tmp_name'];
+if (isset($name)) {
+    if (!empty($name)) 
+    {
+    $location = 'ens/';
+    if (move_uploaded_file($tmp_name, $location. $name));
+    echo 'Uploaded';
+    }
+    else 
+    {
+        echo 'Please choose a file';
+    }
+
+}
+?>
